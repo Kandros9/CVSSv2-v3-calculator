faeeef28af5936703e5cbb594bc0008207a17b66
evelina_r <evelinka-r@mail.ru>
2020-02-22 02:23:35 +0300

    Add path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import Main from "./components/Main";
+import {Switch, Route, BrowserRouter} from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
       <div>
-          <Main/>
+        <BrowserRouter>
+        <Switch>
+          <Route exact path='/cvss' component={Main}/>
+        </Switch>
+        </BrowserRouter>
       </div>
     );
   }
